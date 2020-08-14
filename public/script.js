aefaa5d069c04c05ab9604d79fcae6b30aafde97
CamilleAbella <camille.abella@hotmail.fr>
2020-08-15 01:01:36 +0200

    change animation of reset, add rotation button, add emojis

--- public/script.js
+++ public/script.js
@@ -1,234 +1,239 @@
 (() => {
-  let
-    session = null,
+  let session = null,
     player = false;
-  
+
   const socket = io();
-  
+
   socket.on("moveTo", moveTo);
   socket.on("reset", reset);
   socket.on("ajuste", ajuste);
   socket.on("eat", eat);
   socket.on("message", log);
-  
+
   document.addEventListener("DOMContentLoaded", () => {
-    
-    socket.once("connection", sessions => {
-      const indication = Object.keys(sessions).length > 0 ?
-        "Veuillez choisir une partie parmi les suivantes ou en créer une." :
-        "Veuillez créer une partie."
-      session = prompt(`${indication}\n
-${Object.keys(sessions).map(name => `id: ${name}, users: ${sessions[name]}`).join('\n')}
-      `.trim())
-      if(session.length === 0)
-        session = "General"
-      socket.emit("login", session)
-      document.getElementById("session").innerText = session
-    })
-    
-    document.getElementById("reset").onclick = () => socket.emit('reset');
-    document.getElementById("field").onkeydown = event => {
-      if(event.key === "Enter") {
-        const emoji = document.getElementById("emoji").value
-        const color = document.getElementById("color").value
-        const text = event.target.value
-        const message = `${emoji} ${text}`
+    socket.once("connection", (sessions) => {
+      const indication =
+        Object.keys(sessions).length > 0
+          ? "Veuillez choisir une partie parmi les suivantes ou en créer une."
+          : "Veuillez créer une partie.";
+      session = prompt(
+        `${indication}\n
+${Object.keys(sessions)
+  .map((name) => `id: ${name}, users: ${sessions[name]}`)
+  .join("\n")}
+      `.trim()
+      );
+      if (session.length === 0) session = "General";
+      socket.emit("login", session);
+      document.getElementById("session").innerText = session;
+    });
+
+    document.getElementById("reset").onclick = () => socket.emit("reset");
+    document.getElementById("rotate").onclick = rotate;
+    document.getElementById("field").onkeydown = (event) => {
+      if (event.key === "Enter") {
+        const emoji = document.getElementById("emoji").value;
+        const color = document.getElementById("color").value;
+        const text = event.target.value;
+        const message = `${emoji} ${text}`;
         socket.emit("message", message, color);
-        event.target.value = ""
+        event.target.value = "";
       }
-    }
-    
-    socket.on("userCountUpdate", userCount => {
-      document.getElementById("userCount").innerText = `👁️ ${Math.max(0, userCount - 2)} viewers | ♟️ ${Math.min(2, userCount)} players`
-    })
-    
+    };
+
+    socket.on("userCountUpdate", (userCount) => {
+      document.getElementById("userCount").innerText = `👁️ ${Math.max(
+        0,
+        userCount - 2
+      )} viewers | ♟️ ${Math.min(2, userCount)} players`;
+    });
+
     socket.on("player", (isPlayer) => {
-      player = isPlayer
+      player = isPlayer;
       if (!player) {
-        document.querySelectorAll("#panel > button, #panel > label, #panel > input").forEach(e => {
-          e.style.display = "none"
-        })
+        document
+          .querySelectorAll("#panel > button, #panel > label, #panel > input")
+          .forEach((e) => {
+            e.style.display = "none";
+          });
       }
-    })
-    
+    });
+
     socket.on("boardRequest", () => {
-      socket.emit("boardResponse", document.getElementById("board").innerHTML)
-    })
-    
-    socket.on("boardUpdate", board => {
-      document.getElementById("board").innerHTML = board
-      Array.from(document.getElementById("board").children).forEach(e => {
-        e.onclick = onPieceClick
-      })
-    })
-    
-    socket.once("init", reset)
-    
-    const boardElement = document.getElementById("board")
-    
-    boardElement.onclick = event => {
-      if (!player) return
-      const drag = document.getElementsByClassName("drag")[0]
+      socket.emit("boardResponse", document.getElementById("board").innerHTML);
+    });
+
+    socket.on("boardUpdate", (board) => {
+      document.getElementById("board").innerHTML = board;
+      Array.from(document.getElementById("board").children).forEach((e) => {
+        e.onclick = onPieceClick;
+      });
+    });
+
+    socket.once("init", reset);
+
+    const boardElement = document.getElementById("board");
+
+    boardElement.onclick = (event) => {
+      if (!player) return;
+      const drag = document.getElementsByClassName("drag")[0];
       if (drag && event.target.id === "board") {
-        socket.emit("moveTo", drag.id, {
-          x: event.x,
-          y: event.y
-        })
+        socket.emit("moveTo", drag.id, fixMousePosition(event));
       }
-    }
-    
-    ;['b', 'w'].forEach(team => {
-      'BBKNNPPPPPPPPQRR'.split('').forEach((piece, index) => {
-        const pieceElement = document.createElement('img')
-        pieceElement.src = `img/${team}${piece}.png`
-        pieceElement.id = team + piece + index
-        pieceElement.onclick = onPieceClick
-        boardElement.appendChild(pieceElement)
-      })
-    })
-  })
-  
+    };
+    ["b", "w"].forEach((team) => {
+      "BBKNNPPPPPPPPQRR".split("").forEach((piece, index) => {
+        const pieceElement = document.createElement("img");
+        pieceElement.src = `img/${team}${piece}.png`;
+        pieceElement.id = team + piece + index;
+        pieceElement.onclick = onPieceClick;
+        boardElement.appendChild(pieceElement);
+      });
+    });
+  });
+
   function ajuste(id) {
-    const pieceElement = document.getElementById(id)
-    pieceElement.style.left = (Math.floor((parseInt(pieceElement.style.left, 10) + 40) / 80) * 80) + "px"
-    pieceElement.style.top = (Math.floor((parseInt(pieceElement.style.top, 10) + 40) / 80) * 80) + "px"
+    const pieceElement = document.getElementById(id);
+    pieceElement.style.left =
+      Math.floor((parseInt(pieceElement.style.left, 10) + 40) / 80) * 80 + "px";
+    pieceElement.style.top =
+      Math.floor((parseInt(pieceElement.style.top, 10) + 40) / 80) * 80 + "px";
   }
-  
+
   /**
    * Remet les pièces à leur place d'origine
    */
   function reset() {
-    log(`Reset.`)
-    document.querySelectorAll("#trash > *").forEach(e => {
-      e.classList.remove("eaten")
-      document.getElementById("board").appendChild(e)
-    })
-    
-    ;['b', 'w'].forEach(team => {
-      'BBKNNPPPPPPPPQRR'.split('').forEach((piece, index) => {
-        const pieceElement = document.getElementById(team + piece + index)
-        let y = team === 'b' ? 120 : 520
-        y += piece === 'P' ? 0 : 80 * (team === 'b' ? -1 : 1)
-        pieceElement.style.top = (y - 40) + 'px'
-      })
-    })
-    
-    setTimeout(() => {
-      ;['b', 'w'].forEach(team => {
-        let realIndex = 0
-        'BBKNNPPPPPPPPQRR'.split('').forEach((piece, index) => {
-          if (piece !== "P") return
-          const pieceElement = document.getElementById(team + piece + index)
-          pieceElement.style.left = (realIndex * 80) + 'px'
-          realIndex++
-        })
-      })
-    }, 500)
-    
-    setTimeout(() => {
-      ;['b', 'w'].forEach(team => {
-        'BBKNNPPPPPPPPQRR'.split('').forEach((piece, index) => {
-          if (piece === "P") return
-          const pieceElement = document.getElementById(team + piece + index)
-          
-          let x = 80
-          
-          switch (index) {
-            case 0:
-              x *= 2
-              break
-            case 1:
-              x *= 5
-              break
-            case 2:
-              x *= 4
-              break
-            case 4:
-              x *= 6
-              break
-            case 13:
-              x *= 3
-              break
-            case 14:
-              x *= 0
-              break
-            case 15:
-              x *= 7
-              break
-          }
-          
-          pieceElement.style.left = x + 'px'
-        })
-      })
-    }, 1000)
+    log(`Reset.`);
+
+    // empty trash
+    document.querySelectorAll("#trash > *").forEach((e) => {
+      e.classList.remove("eaten");
+      document.getElementById("board").appendChild(e);
+    });
+
+    ["b", "w"].forEach((team) => {
+      let pionIndex = 0;
+
+      "BBKNNPPPPPPPPQRR".split("").forEach((piece, index) => {
+        const pieceElement = document.getElementById(team + piece + index);
+
+        let x = 80,
+          y;
+
+        switch (index) {
+          case 0:
+            x *= 2;
+            break;
+          case 1:
+            x *= 5;
+            break;
+          case 2:
+            x *= 4;
+            break;
+          case 3:
+            break;
+          case 4:
+            x *= 6;
+            break;
+          case 13:
+            x *= 3;
+            break;
+          case 14:
+            x *= 0;
+            break;
+          case 15:
+            x *= 7;
+            break;
+          default:
+            x *= pionIndex;
+        }
+
+        y = team === "b" ? 120 : 520;
+        y += piece === "P" ? 0 : 80 * (team === "b" ? -1 : 1);
+
+        pieceElement.style.top = y - 40 + "px";
+        pieceElement.style.left = x + "px";
+
+        if (piece === "P") pionIndex++;
+      });
+    });
   }
-  
+
+  function rotate() {
+    document.getElementById("board").classList.toggle("rotate");
+  }
+
   function moveTo(id, position, disableLog) {
-    const pieceElement = document.getElementById(id)
-    if(!disableLog) log(`${icon(pieceElement)} move.`)
+    const pieceElement = document.getElementById(id);
+    if (!disableLog) log(`${icon(pieceElement)} move.`);
     // move drag on target position
-    pieceElement.style.left = (position.x - 40) + "px"
-    pieceElement.style.top = (position.y - 40) + "px"
+    pieceElement.style.left = position.x - 40 + "px";
+    pieceElement.style.top = position.y - 40 + "px";
     // remove focus
-    pieceElement.classList.remove("drag")
+    pieceElement.classList.remove("drag");
     // ajuste
-    socket.emit("ajuste", pieceElement.id)
+    socket.emit("ajuste", pieceElement.id);
   }
-  
+
   function eat(eaterId, id) {
-    const pieceElement = document.getElementById(id)
-    const eaterElement = document.getElementById(eaterId)
-    log(`${icon(eaterElement)} eat ${icon(pieceElement)}.`)
-    pieceElement.classList.add("eaten")
+    const pieceElement = document.getElementById(id);
+    const eaterElement = document.getElementById(eaterId);
+    log(`${icon(eaterElement)} eat ${icon(pieceElement)}.`);
+    pieceElement.classList.add("eaten");
     document.getElementById("trash").appendChild(pieceElement);
   }
-  
+
   function onPieceClick(event) {
-    if (!player) return
-    if(event.target.classList.contains("eaten")){
-      event.target.classList.remove("eaten")
-      document.getElementById("board").appendChild(event.target)
-      return
+    if (!player) return;
+    if (event.target.classList.contains("eaten")) {
+      event.target.classList.remove("eaten");
+      document.getElementById("board").appendChild(event.target);
+      return;
     }
-    const drag = document.getElementsByClassName("drag")[0]
+    const drag = document.getElementsByClassName("drag")[0];
     if (drag) {
       if (drag === event.target) {
-        drag.classList.remove("drag")
+        drag.classList.remove("drag");
       } else {
         if (drag.id[0] !== event.target.id[0]) {
           // eat target
-          socket.emit("eat", drag.id, event.target.id)
+          socket.emit("eat", drag.id, event.target.id);
         } else {
-          drag.classList.remove("drag")
-          return
+          drag.classList.remove("drag");
+          return;
         }
-        socket.emit("moveTo", drag.id, {
-          x: event.x,
-          y: event.y
-        }, true)
+        socket.emit("moveTo", drag.id, fixMousePosition(event), true);
       }
     } else {
-      event.target.classList.add("drag")
+      event.target.classList.add("drag");
     }
   }
-  
-  function shifting(x, shift) {
-    shift = (Math.random() * shift * 2) - shift
-    return x + shift
+
+  function log(text, color) {
+    const div = document.createElement("div");
+    div.innerHTML = text;
+    if (color) div.style.color = color;
+    document.getElementById("console").appendChild(div);
   }
-  
-  function log(text, color){
-    const div = document.createElement("div")
-    div.innerHTML = text
-    if(color) div.style.color = color
-    document.getElementById("console").appendChild(div)
-  }
-  
-  function icon(pieceElement){
+
+  function icon(pieceElement) {
     return `<img
       src="${pieceElement.src}"
       height="15"
-      alt="${pieceElement.id.slice(0,2)}"
-      title="${pieceElement.id.slice(0,2)}">`
+      alt="${pieceElement.id.slice(0, 2)}"
+      title="${pieceElement.id.slice(0, 2)}">`;
+  }
+
+  function fixMousePosition(position) {
+    const newPosition = {
+      x: position.x,
+      y: position.y,
+    };
+    if (document.querySelector("#board.rotate")) {
+      newPosition.x = 640 - newPosition.x;
+      newPosition.y = 640 - newPosition.y;
+    }
+    return newPosition;
   }
 })();
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
 
@@ -6,80 +6,85 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const sessions = {};
 
-io.on("connection", socket => {
-  
-  const sessionsInfo = {}
-  for(const name in sessions)
-    sessionsInfo[name] = sessions[name].length
-  
-  socket.emit("connection", sessionsInfo)
-  
-  socket.on("boardResponse", board => {
-    sessions[socket.session][sessions[socket.session].length-1]
-      .emit("boardUpdate", board)
-  })
-  
-  socket.on("login", session => {
-    
-    socket.session = session
-    if(!sessions[session]){
-      sessions[session] = [socket]
-    }else{
-      sessions[session].push(socket)
+io.on("connection", (socket) => {
+  const sessionsInfo = {};
+  for (const name in sessions) sessionsInfo[name] = sessions[name].length;
+
+  socket.emit("connection", sessionsInfo);
+
+  socket.on("boardResponse", (board) => {
+    sessions[socket.session][sessions[socket.session].length - 1].emit(
+      "boardUpdate",
+      board
+    );
+  });
+
+  socket.on("login", (session) => {
+    socket.session = session;
+    if (!sessions[session]) {
+      sessions[session] = [socket];
+    } else {
+      sessions[session].push(socket);
     }
-    
-    const length = sessions[session].length
-    if(length > 1){
-      sessions[session][0].emit("boardRequest")
-    }else{
-      socket.emit("init")
+
+    const length = sessions[session].length;
+    if (length > 1) {
+      sessions[session][0].emit("boardRequest");
+    } else {
+      socket.emit("init");
     }
-    
-    socket.emit("player", sessions[session].length < 3)
-    
-    socket.join(session)
-  
-    socket.emit("userCountUpdate", sessions[session].length)
-    socket.broadcast.to(session).emit("userCountUpdate", sessions[session].length)
-  
+
+    socket.emit("player", sessions[session].length < 3);
+
+    socket.join(session);
+
+    socket.emit("userCountUpdate", sessions[session].length);
+    socket.broadcast
+      .to(session)
+      .emit("userCountUpdate", sessions[session].length);
+
     socket.on("disconnect", () => {
-      sessions[socket.session] = sessions[socket.session].filter(s => s !== socket)
-      socket.broadcast.to(socket.session).emit("userCountUpdate", sessions[session].length)
-      if(sessions[socket.session].length === 0)
-        delete sessions[socket.session]
-    })
-  
+      sessions[socket.session] = sessions[socket.session].filter(
+        (s) => s !== socket
+      );
+      socket.broadcast
+        .to(socket.session)
+        .emit("userCountUpdate", sessions[session].length);
+      if (sessions[socket.session].length === 0)
+        delete sessions[socket.session];
+    });
+
     socket.on("moveTo", (id, pos, dLog) => {
-      socket.emit("moveTo", id, pos, dLog)
-      socket.broadcast.to(socket.session).emit("moveTo", id, pos, dLog)
-    })
-  
+      socket.emit("moveTo", id, pos, dLog);
+      socket.broadcast.to(socket.session).emit("moveTo", id, pos, dLog);
+    });
+
     socket.on("reset", () => {
-      socket.emit("reset")
-      socket.broadcast.to(socket.session).emit("reset")
-    })
-  
+      socket.emit("reset");
+      socket.broadcast.to(socket.session).emit("reset");
+    });
+
     socket.on("ajuste", (id) => {
-      socket.emit("ajuste", id)
-      socket.broadcast.to(socket.session).emit("ajuste", id)
-    })
-  
+      socket.emit("ajuste", id);
+      socket.broadcast.to(socket.session).emit("ajuste", id);
+    });
+
     socket.on("eat", (eaterId, id) => {
-      socket.emit("eat", eaterId, id)
-      socket.broadcast.to(socket.session).emit("eat", eaterId, id)
-    })
-    
+      socket.emit("eat", eaterId, id);
+      socket.broadcast.to(socket.session).emit("eat", eaterId, id);
+    });
+
     socket.on("message", (text, color) => {
-      socket.emit("message", text, color)
-      socket.broadcast.to(socket.session).emit("message", text, color)
-    })
-  })
-})
+      socket.emit("message", text, color);
+      socket.broadcast.to(socket.session).emit("message", text, color);
+    });
+  });
+});
 
 server.listen(3000, () => {
-  console.log('listening on localhost:3000');
+  console.log("listening on localhost:3000");
 });
